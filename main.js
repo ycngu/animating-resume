71b0e7eaa375307563024d307ccf04a561e2edef
ycngu <snl123698745@163.com>
2018-12-14 10:56:46 +0800

    ver 1.0

---
+++ main.js
@@ -0,0 +1,30 @@
+content = `/*
+* 面试官你好，我是ycngu
+* 我将以动画的形式介绍自己
+* 接下来就是写代码的时间了
+* 首先，我要准备一些样式
+*/
+
+* {
+    transition: all 1s;
+}
+html {
+    background: rgb(222,222,222);
+    font-size: 16px;
+    padding: 16px;
+}
+#code {
+    border:1px solid blue;
+}
+`
+
+var n = 0
+var id = setInterval(() => {
+   n+=1
+   code.innerHTML = content.substring(0, n) 
+   styleTag.innerHTML = content.substring(0, n) 
+   if(n >= content.length){
+       clearInterval(id)
+   }
+}, 10)
+
