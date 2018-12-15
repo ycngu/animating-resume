52a429f4e058514396656362a75e238707bd480c
ycngu <snl123698745@163.com>
2018-12-15 22:18:20 +0800

    增加代码高亮

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 content = `/*
-* 面试官你好，我是ycngu
-* 接下来我将以动画的形式介绍自己
-* 下面就是写代码的时间了
-* 首先，我要准备一些样式
-*/
+ *  面试官你好，我是ycngu
+ *  接下来我将以动画的形式介绍自己
+ *  下面就是写代码的时间了
+ *  首先，我要准备一些样式
+ */
 
 * {
     transition: all 1s;
@@ -14,7 +14,31 @@ html {
     padding: 16px;
 }
 #code {
-    border:1px solid blue;
+    border: 1px solid white;
+    box-shadow: 0px 0px 10px #000000;
+    padding: 10px;
+}
+
+/* 接下来我需要一点代码高亮 */
+
+.token.comment{
+    color: slategray;
+}
+
+.token.selector{
+    color: #690;
+} 
+
+.token.property{
+    color: #905;
+}
+
+.token.function{
+    color: #DD4A68;
+}
+
+.token.punctuation{
+    color: #999;
 }
 `
 
