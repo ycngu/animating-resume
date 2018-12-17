c1081ba195f7bd7c2eb7c8fc925899d6b18f34ed
ycngu <snl123698745@163.com>
2018-12-17 21:45:20 +0800

    添加滑块样式

--- main.js
+++ main.js
@@ -24,21 +24,31 @@ html {
 .token.comment{
     color: slategray;
 }
-
 .token.selector{
     color: #690;
 } 
-
 .token.property{
     color: #905;
 }
-
 .token.function{
     color: #DD4A68;
 }
-
 .token.punctuation{
     color: #999;
+}
+/* 好了，准备工作完成 */
+/* 接下来正式写简历 */
+
+/* 首先准备一张白纸 */
+#code{
+    display:inline-block;
+    width:45%;
+}
+
+#paper {
+    display:inline-block;
+    background-color:white;
+    width:50%;
 }
 `
 
@@ -48,14 +58,17 @@ var id = setInterval(() => {
    code.innerHTML = content.substring(0, n)
    code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css, 'css')
    styleTag.innerHTML = content.substring(0, n) 
-   scroll(0, 1000)
+   code.scroll(0, 1000)
    if(n >= content.length){
        clearInterval(id)
        fn2()
    }
 }, 10)
 
+content2 =`/* 接下来正式写简历 */
+
+`
 
 function fn2(){
-    
+
 }
