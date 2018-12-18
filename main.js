3d4d381b35d4f8257f522abc4e8afcb134c80596
ycngu <snl123698745@163.com>
2018-12-18 14:28:41 +0800

    重构了下代码，开始写右边简历

--- main.js
+++ main.js
@@ -49,26 +49,56 @@ html {
     display:inline-block;
     background-color:white;
     width:50%;
+    border:5px solid #444;
 }
+
+/* 现在请看右边 */
+`
+
+content2 = `
+
 `
 
 var n = 0
-var id = setInterval(() => {
-   n+=1
-   code.innerHTML = content.substring(0, n)
-   code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css, 'css')
-   styleTag.innerHTML = content.substring(0, n) 
-   code.scroll(0, 1000)
-   if(n >= content.length){
-       clearInterval(id)
-       fn2()
-   }
-}, 10)
+writeCode('#code', content, writeCode('#paper', content2, fn2))
+// var id = setInterval(() => {
+//     n += 1
+//     code.innerHTML = content.substring(0, n)
+//     code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css, 'css')
+//     styleTag.innerHTML = content.substring(0, n)
+//     code.scroll(0, 1000)
+//     if (n >= content.length) {
+//         clearInterval(id)
+//         fn2()
+//     }
+// }, 10)
 
-content2 =`/* 接下来正式写简历 */
+function fn2() {
+    // setInterval(() => {
+    //     n += 1
+    //     code.innerHTML = content.substring(0, n)
+    //     code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css, 'css')
+    //     styleTag.innerHTML = content.substring(0, n)
+    //     code.scroll(0, 1000)
+    //     if (n >= content.length) {
+    //         clearInterval(id)
+    //         fn2()
+    //     }
+    // }, 10)
+}
 
-`
-
-function fn2(){
-
+function writeCode(view, content, callback) {
+    let n = 0
+    let id = setInterval(() => {
+        n += 1
+        dom = document.querySelector(view)
+        dom.innerHTML = content.substring(0, n)
+        dom.innerHTML = Prism.highlight(dom.innerHTML, Prism.languages.css, 'css')
+        styleTag.innerHTML = content.substring(0, n)
+        dom.scroll(0, 1000)
+        if (n >= content.length) {
+            clearInterval(id)
+            callback && callback.call()
+        }
+    }, 10)
 }
